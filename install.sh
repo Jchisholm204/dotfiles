5606f6ec92067fef0d943bcd37f5771ece2b1d50
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-12-27 10:32:02 -0800

    tmux_install

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
+# Dotfiles Install Script
+# Jchisholm204.github.io
+
+# Get the distro ID (for apt install)
+distro_id=$(cat /etc/*release | grep "^ID=" | cut -d'=' -f2)
 
 echo "Installing JC204 Config"
 C_DIR=$(pwd)
 echo "Installing from $C_DIR"
+
+echo "Have git submodules been initialized? [y/n]"
+read -r ans
+if [ "$ans" == "n" ]; then
+    echo "Git submodules must be initialized..Doing it now"
+    git submodule init
+    git submodule update
+fi
 
 echo "Setting up Fonts"
 
@@ -30,15 +43,58 @@ echo "Installed Fonts"
 echo "Set up ZSH? [y/n]:"
 read -r ans
 if [ "$ans" == "y" ]; then
+    if [ "$distro_id" = "fedora" ]; then
+        sudo dnf install zsh
+    elif [ "$distro_id" = "ubuntu" ]; then
+        sudo apt-get install zsh
+    elif [ "$distro_id" = "archlinux" ]; then
+        pacman -S zsh
+    fi
     echo "Install OhMyZsh? [y/n]"
     read -r ans
     if [ "$ans" == "y" ]; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        echo "Cloning zsh-autosuggestions"
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     fi
     if [ ! -d  "$HOME/.zshrc" ]; then
-        echo "Found OLD ZSH Config: renamed to zshrc-old"
-        mv "$HOME"/.zshrc "$HOME"/zshrc-old
+        echo "Found OLD ZSH Config: renamed to zshrc.bak"
+        mv "$HOME"/.zshrc "$HOME"/zshrc.bak
     fi
     ln -s $(pwd)/.zshrc "$HOME"
     echo "Created ZSHrc Symlink"
 fi
+
+echo "Set up TMUX [y/n]"
+read -r ans
+if [ "$ans" == "y" ]; then
+    if [ ! $(ls /bin | grep tmux) == "tmux"]; then
+        echo "Attempting to install tmux"
+        if [ "$distro_id" = "fedora" ]; then
+            sudo dnf install tmux
+        elif [ "$distro_id" = "ubuntu" ]; then
+            sudo apt-get install tmux
+        elif [ "$distro_id" = "archlinux" ]; then
+            pacman -S tmux
+        fi
+    else
+        echo "TMUX Installation Found"
+    fi
+    echo "Looking for old TMUX Config"
+    if [ ! -d "$HOME/.tmux.conf" ]; then
+        echo "Found an old TMUX Config"
+        echo "Would you like to backup? [y/n]"
+        read -r ans
+        if [ "$ans" == "y" ]; then
+            mv "$HOME/.tmux.conf" "$HOME/tmux.conf.bak"
+        else
+            rm "$HOME/.tmux.conf"
+        fi
+    fi
+    ln -s $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
+    tmux source $HOME/.tmux.conf
+fi
+    
+echo "Install Completed."
+
+
